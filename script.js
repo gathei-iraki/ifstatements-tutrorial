3bfd51e94d6b33e0dcb72b3a4735b1c2009d1899
gathei-iraki <njorogegathei@gmail.com>
2023-09-06 15:29:05 +0300

    Create script.js
    added js file

---
+++ script.js
@@ -0,0 +1,29 @@
+// IF STATEMENT
+const age=18;
+if(age>18){
+console.log("You may enter");
+}
+else if(age===18){
+    console.log(`You just turned ${age}. Welcome!`);
+}
+else{
+    console.log("Grow up!");
+}
+
+
+const cats=5;
+if(!cats){
+    console.log(`I have ${cats} cats` );
+}
+else{
+    console.log("Buy more cats");
+}
+
+const dogs=8;
+if(dogs){
+    console.log("False");
+
+}
+else{
+    console.log("true");
+}
